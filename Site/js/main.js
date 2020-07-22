c0b7e35eabc2b16124c6f3243b04d04a2abaff0a
Dmitriy <dyrda.dmitrij@mail.ru>
2020-07-23 01:57:13 +0300

    sliderIndex

---
+++ Site/js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+    $('.slider_items').slick({
+        nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+        infinite: false
+    })
+});
